1c9f03d82c2175493e3a5ba56e54f9c42c3ac502
Mathias Jensen <mathiasbj94@gmail.com>
2017-03-21 00:25:45 +0100

    skod

--- Softwarehuset/src/Employee.java
+++ Softwarehuset/src/Employee.java
@@ -2,7 +2,7 @@
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Skod");
-		System.out.println("Mere skod");
+		System.out.println("Mere skod blallalala");
 	}
 
 }
