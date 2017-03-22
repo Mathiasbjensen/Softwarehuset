5a121db0f604b0ae952d94e4c11bcaba64c68b4e
s164158 <s164158@student.dtu.dk>
2017-03-22 12:35:23 +0100

    Har tilføjet en linje af Sebastian

--- Softwarehuset/src/Employee.java
+++ Softwarehuset/src/Employee.java
@@ -3,6 +3,7 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Skod");
 		System.out.println("Mere skod blallala");
+		System.out.prinntln("Skrevet af Sebastian")
 	}
 
 }
