224517fedea2709c9bc6e0e7f8143e1a70b0f54e
Mathias Jensen <Mathiasbj94@gmail.com>
2017-03-27 10:35:38 +0200

    Testing

--- Softwarehuset/src/Employee.java
+++ Softwarehuset/src/Employee.java
@@ -3,7 +3,8 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("Skod");
 		System.out.println("Mere skod blallala");
-		System.out.prinntln("Skrevet af Sebastian")
+		System.out.prinntln("Skrevet af Sebastian");
+		System.out.println("Test")
 	}
 
 }
