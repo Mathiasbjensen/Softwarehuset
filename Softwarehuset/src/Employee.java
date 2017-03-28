6467276f9bdf180a5af81953fef41e3d6bbc730b
Mathias Jensen <mathiasbj94@gmail.com>
2017-03-28 12:05:34 +0200

    Fixed merging issues

--- Softwarehuset/src/Employee.java
+++ Softwarehuset/src/Employee.java
@@ -2,43 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Employee {
-<<<<<<< HEAD
 	private String ID;
 	private String Name;
 	private List<Employee> Employees = new ArrayList<Employee>();
 	
-
+	
 	public Employee(String ID) {
-		this.Name = Name;
 		this.ID = ID;
 	}
 
+	// Har vi brug for denne metode????
 	public String getName() {
 		return Name;
-=======
-	private String ID; //navn (initials?)
-	
-	
-	public User(String ID){ //employee
-		this.ID = ID;
 		
 		
 	}
-	
-	public String GetID(){ // Returner intialier på User 
+	// Returner intialier på User 
+	public String GetID(){ 
 		return ID;
 	}
 
-
-
-	public static void main(String[] args) {
-
->>>>>>> fcc4a03cded7729ffea13a6179650a542e3bf79a
-	}
-
-	public String getID() {
-		return ID;
-	}
 
 
 	public List<Employee> getEmployees() {
