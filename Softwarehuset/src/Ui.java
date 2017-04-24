ab11a53798e7c739e0f4d7bfaf2b9824e7561c66
Mathias Jensen <Mathiasbj94@gmail.com>
2017-04-24 10:21:59 +0200

    Added ui class

---
+++ Softwarehuset/src/Ui.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Ui extends JFrame implements ActionListener {
+	
+	private static JButton project, addProject,; 
+
+	public Ui() {
+		getContentPane().setLayout(new BorderLayout());
+		
+		// Creating a project button
+		project = new JButton("Projects");
+		project.addActionListener(this);
+		project.setMaximumSize(new Dimension(200,200));
+		project.setAlignmentX(Component.CENTER_ALIGNMENT);
+		project.setAlignmentY(Component.CENTER_ALIGNMENT);
+		
+		JPanel buttonpanel = new JPanel();
+		buttonpanel.setMinimumSize(new Dimension(700,700));
+		buttonpanel.setMaximumSize(new Dimension(700,700));
+
+		buttonpanel.setLayout(new BoxLayout(buttonpanel, BoxLayout.Y_AXIS));
+//		buttonpanel.setBackground(new Color(255, 0, 0));
+		buttonpanel.add(project);
+		
+		
+		// Creating a button to add projects
+		addProject = new JButton("Add Project");
+		addProject.addActionListener(this);
+		addProject.setMaximumSize(new Dimension(200,200));
+		addProject.setAlignmentX(Component.CENTER_ALIGNMENT);
+		addProject.setAlignmentY(Component.CENTER_ALIGNMENT);
+		buttonpanel.add(addProject);
+		
+		
+		
+		
+		
+		
+		getContentPane().add(buttonpanel, BorderLayout.CENTER);
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		Ui test = new Ui();
+
+//		test.setUndecorated(true);
+		test.setTitle("Softwarehuset");
+		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		test.pack();
+		test.setVisible(true);
+		test.setSize(700,700);
+		test.setResizable(false);
+	
+	}
+	
+}
