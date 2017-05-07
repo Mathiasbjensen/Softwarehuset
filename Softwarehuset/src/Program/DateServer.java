6dcd80f477896f010ced375c7f2e51ed9b060d39
Frederik Kirkegaard <frederik-kkirkegaard@hotmail.com>
2017-05-07 13:37:32 +0200

    Slettet ting for coverage

--- Softwarehuset/src/Program/DateServer.java
+++ Softwarehuset/src/Program/DateServer.java
@@ -2,19 +2,6 @@ package Program;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 public class DateServer {
-	public static void main(String[] args){
-//		Calendar.getInstance();
-		Calendar calendar = new GregorianCalendar();
-		int year = Calendar.getInstance().get(Calendar.YEAR);
-		
-		System.out.println(year);
-		int week = Calendar.getInstance().get(Calendar.WEEK_OF_YEAR);
-		System.out.println(week);
-		
-		int day = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
-		System.out.println(day);
-		
-	}
 	
 	public int getYear() {
 		int year = Calendar.getInstance().get(Calendar.YEAR);
